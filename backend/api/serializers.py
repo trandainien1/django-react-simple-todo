b9d6cfa1b2f5e937234b494aac32bd0871f93017
trandainien1 <trandainien1@gmail.com>
2024-11-09 14:20:49 +0700

    set up url for list note and delete note

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from .models import Note
+
 # serializer will look at the input of User. Then it will check the validation of input. Then it will call create() with validated data
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +15,11 @@ class UserSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
+        return user
+
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at', 'author']
+        extra_kwargs = {'author': {'read_only': True}}
+
